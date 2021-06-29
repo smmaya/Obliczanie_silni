e61907ad1ab104bfe1612b4604a800fb4a205a98
Sławomir Majchrzak <52676366+smmaya@users.noreply.github.com>
2021-06-29 12:44:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Oblicz_Silnię
+{
+    /// Silnia.
+    class Silnia
+    {
+        /// Silnia rekurencyjnie
+        private static int silnia1(int i)
+        {
+            if (i < 1)
+                return 1;
+            else
+                return i * silnia1(i - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            String line;
+            int n = 0;
+
+            Console.WriteLine("Podaj dla jakiej liczby obliczyć silnię: \n");
+            line = Console.ReadLine();
+            n = int.Parse(line);
+
+            //Console.WriteLine(n + "! = " + silnia1(5));
+            Console.WriteLine(n + "! = " + silnia1(n));
+            Console.ReadKey();
+        }
+       
+    }
+}
